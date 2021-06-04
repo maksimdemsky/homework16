ba699043ab1d32607708935a6b63994afbcddd28
maksim <maksimdemsky@gmail.com>
2021-06-04 14:04:50 +0300

    add buy smartphone test with POST

--- src/test/java/tests/WebShopTests.java
+++ src/test/java/tests/WebShopTests.java
@@ -1,4 +1,22 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
 public class WebShopTests {
+
+    @Test
+    void addItemToCardTest() {
+        given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .body("addtocart_43.EnteredQuantity=1")
+                .when()
+                .post("http://demowebshop.tricentis.com/addproducttocart/details/43/1")
+                .then()
+                .statusCode(200)
+                .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"));
+    }
 }
